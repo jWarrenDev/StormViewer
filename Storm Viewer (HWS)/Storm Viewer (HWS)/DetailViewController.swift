8d59d2c4fa54029f3a80030ef9efaa299e29f002
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-21 11:42:04 -0700

    Created DetailViewController with UIimage

---
+++ Storm Viewer (HWS)/Storm Viewer (HWS)/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer (HWS)
+//
+//  Created by Jerrick Warren on 7/21/19.
+//  Copyright © 2019 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
