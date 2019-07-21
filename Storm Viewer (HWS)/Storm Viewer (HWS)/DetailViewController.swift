44107e60aab1d5898429874d620ac19499bd5c31
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-21 16:15:00 -0700

    MVP Storm Viewer Finished

--- Storm Viewer (HWS)/Storm Viewer (HWS)/DetailViewController.swift
+++ Storm Viewer (HWS)/Storm Viewer (HWS)/DetailViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
 
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
-    @IBOutlet weak var imageView: UIImageView!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+   
+    
+    override func viewWillDisappear(_ animated: Bool) {
+       super.viewWillDisappear(animated)
+       navigationController?.hidesBarsOnTap = false
+    }
+    
     
     /*
     // MARK: - Navigation
