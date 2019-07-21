72925fceccb87f5bf2dbed26bc6728fa9139c521
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-21 11:33:07 -0700

    added delegate functions for table View

--- Storm Viewer (HWS)/Storm Viewer (HWS)/TableViewController.swift
+++ Storm Viewer (HWS)/Storm Viewer (HWS)/TableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class ViewController: UITableViewController {
 
     var pictures = [String]()
     
@@ -29,6 +29,18 @@ class TableViewController: UITableViewController {
             }
         }
         print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
     }
 }
 
